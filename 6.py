780e7c0c9ab5facc53dd53f944ff971b67af8d01
veratrum <veratrum@users.noreply.github.com>
2020-12-07 20:05:07 +1300

    Days 6 and 7

---
+++ 6.py
@@ -0,0 +1,30 @@
+
+import re
+
+with open('input\\6.txt', 'r') as f:
+    groups = [x.split('\n') for x in f.read().split('\n\n')]
+
+total = 0
+for group in groups:
+    answered = set()
+    count = 0
+    for person in group:
+        for letter in person:
+            if letter not in answered:
+                answered.add(letter)
+                count += 1
+    total += count
+
+print(total)
+
+
+total = 0
+for group in groups:
+    answered = set(group[0])
+    for person in group[1:]:
+        for letter in set(answered):
+            if letter not in person:
+                answered.remove(letter)
+    total += len(answered)
+
+print(total)
---
+++ 7.py
@@ -0,0 +1,40 @@
+
+import re
+
+with open('input\\7.txt', 'r') as f:
+    lines = f.readlines()
+
+bags = []
+bagsDict = {}
+for line in lines:
+    name = re.search(r'^(.*) bags contain', line).group(1)
+    remainder = line.partition('contain ')[2].strip('.\n')
+    if remainder == 'no other bags':
+        inside = []
+    else:
+        inside = [(int(re.search(r'^(\d+) (.*) bags?$', segment).group(1)), re.search(r'^(\d+) (.*) bags?$', segment).group(2)) for segment in remainder.split(', ')]
+    bags.append((name, inside))
+    bagsDict[name] = inside
+
+found = set(['shiny gold'])
+oldFound = set()
+while found != oldFound:
+    oldFound = set(found)
+    for bag, inside in bags:
+        insideBags = set(x[1] for x in inside)
+        if bag in found:
+            continue
+        if not insideBags.isdisjoint(found):
+            found.add(bag)
+
+print(len(found) - 1)
+
+def countBags(name):
+    inside = bagsDict[name]
+    total = 1
+    for count, insideName in inside:
+        total += countBags(insideName) * count
+    print(f'{name}: {total}')
+    return total
+
+print(countBags('shiny gold') - 1)
